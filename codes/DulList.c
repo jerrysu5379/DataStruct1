7d07ac1aac843453ab4ce12f6db0752f0028dc8e
jerrysu5379 <jerrysu5379@gmail.com>
2024-03-12 15:29:19 +0800

    update

---
+++ codes/DulList.c
@@ -0,0 +1,59 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef int DataType;
+
+typedef struct DulNode {
+    DataType data;
+    struct DulNode *prior, *next;
+} Node;
+
+int InitList(Node **L) {
+    *L = (Node *)malloc(sizeof(Node));
+    if (*L == NULL) {
+        return 0;
+    }
+    (*L)->prior = *L;
+    (*L)->next = *L;
+    return 1;
+}
+
+int ListInsert(Node *L, int i, DataType e) {
+    Node *p = L;
+    int j = 0;
+    while (p->next != L && j < i - 1) {
+        p = p->next;
+        j++;
+    }
+    if (j != i - 1) {
+        return 0;
+    }
+    Node *s = (Node *)malloc(sizeof(Node));
+    if (s == NULL) {
+        return 0;
+    }
+    s->data = e;
+    s->next = p->next;
+    p->next->prior = s;
+    s->prior = p;
+    p->next = s;
+    return 1;
+}
+
+int ListDelete(Node *L, int i, DataType *e) {
+    Node *p = L;
+    int j = 0;
+    while (p->next != L && j < i - 1) {
+        p = p->next;
+        j++;
+    }
+    if (j != i - 1) {
+        return 0;
+    }
+    Node *q = p->next;
+    *e = q->data;
+    p->next = q->next;
+    q->next->prior = p;
+    free(q);
+    return 1;
+}
