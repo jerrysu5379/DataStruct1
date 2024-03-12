36e3d15c5c8d822d607228d135803ebc5993ad35
jerrysu5379 <jerrysu5379@gmail.com>
2024-03-12 14:54:52 +0800

    update

--- main.c
+++ main.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
+#include "stdio.h"
+#include "stdlib.h"
 
 typedef int DataType;
 
@@ -12,87 +7,56 @@ typedef struct Node {
     DataType data;
     struct Node *next;
 } Node;
-Node *first;
 
-void Init() {
-    first = (Node *) malloc(sizeof(Node));
-    first->next = NULL;
-}
-
-void Insert(Node *first, int i, DataType x) {
-    Node *p = first;
-    int j = 0;
-    while (p != NULL && j < i - 1) {
+int InsertElem(Node *L, int i, DataType *ptr) {
+    int j;
+    Node *p, *s;
+    p = L;
+    j = 0;
+    while (p && j < i - 1) {
         p = p->next;
         j++;
     }
-    if (p == NULL || j > i - 1) {
-        printf("插入位置不合法\n");
-        return;
-    }
-    Node *s = (Node *) malloc(sizeof(Node));
-    s->data = x;
-    s->next = p->next;
-    p->next = s;
-}
-
-void PrintList(Node *first){
-    Node *p = first->next;
-    while (p != NULL) {
-        printf("%d ", p->data);
-        p = p->next;
-    }
-    printf("\n");
-}
-
-int Length(Node *first){
-    Node *p = first->next;
-    int count = 0;
-    while (p != NULL) {
-        count++;
-        p = p->next;
-    }
-    return count;
-}
-
-void Delete(DataType x) {
-    Node *p = first->next;
-    Node *pre = first;
-    while (p != NULL) {
-        if (p->data == x) {
-            pre->next = p->next;
-            free(p);
-            p = pre->next;
-        } else {
-            pre = p;
-            p = p->next;
-        }
-    }
-}
-
-int Get(Node *first, int i, DataType *ptr) {
-    Node *p = first->next;
-    int j = 1;
-    while (p != NULL && j < i) {
-        p = p->next;
-        j++;
-    }
-    if (p == NULL || j > i) {
+    if (!p || j > i - 1) {
         return 0;
     }
-    *ptr = p->data;
+    s = (Node *) malloc(sizeof(Node));
+    s->data = *ptr;
+    s->next = p->next;
+    p->next = s;
     return 1;
 }
 
-int Locate(Node *first, DataType x) {
-    Node *p = first->next;
-    int j = 1;
-    while (p != NULL && p->data != x){
+int DelElem(Node *L, int i, DataType *ptr) {
+    int j;
+    Node *p, *q;
+    q = NULL;
+    p = L;
+    j = 0;
+    while (p->next && j < i - 1) {
         p = p->next;
         j++;
     }
-    if (p == NULL){
+    if (!p->next || j > i - 1) {
         return 0;
     }
-    return j;
+    q = p->next;
+    p->next = q->next;
+    *ptr = q->data;
+    free(q);
+    return 1;
 }
+
+Node *CreateList(DataType a[], int n) {
+    Node *p = NULL;
+    Node *L = (Node *) malloc(sizeof(Node));
+    L->next = NULL;
+    for (int i = 0; i < n; i++) {
+        p = (Node *) malloc(sizeof(Node));
+        p->data = a[i];
+        p->next = L->next;
+        L->next = p;
+    }
+    return L;
+}
+
